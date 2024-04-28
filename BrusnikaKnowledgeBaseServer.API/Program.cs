e2c91356190720e60370c0a24cc208a6b0fae36c
aterasu <obuhowdaniil902@gmail.com>
2024-04-28 15:18:52 +0500

    add database

--- BrusnikaKnowledgeBaseServer.API/Program.cs
+++ BrusnikaKnowledgeBaseServer.API/Program.cs
@@ -1,6 +1,33 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+/*builder.Services.AddSwaggerGen();*/
+
+
+
+/*builder.Services.AddCors(options => options.AddPolicy("CorsPolicy",
+                builder =>
+                {
+                    builder
+                    .WithOrigins("http://localhost:3000")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials();
+                }));
+builder.Services.AddCors();*/
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline.
+
+/*app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Example v1"));
+
+app.UseRouting();
+
+app.UseCors("CorsPolicy");*/
+
+/*app.UseAuthorization();*/
 
 app.Run();
